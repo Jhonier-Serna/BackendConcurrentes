41409189383bb075c40d355e6c3d202d0bf3f001
Jhonier-Serna <jhoniier.serna@gmail.com>
2024-12-15 05:16:08 -0500

    Feature: Inserción con indices en campos de consulta

--- app/services/file_processor.py
+++ app/services/file_processor.py
@@ -18,25 +18,40 @@ class FileProcessorService:
         self.file_storage = FileStorageService()
         self.vcf_parser = VCFParserService()
         self.n_cores = multiprocessing.cpu_count()
-        self.database =  get_async_database()
+        self.database = get_async_database()
         self.genes_collection = self.database.genes
 
+    async def _create_indexes(self):
+        """
+        Crea índices individuales en los campos relevantes para búsquedas con expresiones regulares.
+        """
+        try:
+            # Índices individuales para los campos que se consultarán con expresiones regulares
+            await self.genes_collection.create_index("chromosome", background=True, name="chromosome_regex_index")
+            await self.genes_collection.create_index("filter_status", background=True, name="filter_status_regex_index")
+            await self.genes_collection.create_index("info", background=True, name="info_regex_index")
+            await self.genes_collection.create_index("format", background=True, name="format_regex_index")
+            
+            logger.info("Índices individuales creados para búsquedas con expresiones regulares.")
+        except Exception as e:
+            logger.error(f"Error creando índices para regex: {e}")
+
     async def process_file(
-            self,
-            file: UploadFile,
+        self,
+        file: UploadFile,
     ):
         """
         Main method to process an uploaded file.
         
         :param file: Uploaded file
-        :param file_metadata: File metadata
         :return: Processed file record
         """
         start_time = datetime.now()
+        
+        await self._create_indexes() #Indices
 
         # Save the file
         file_path = await self.file_storage.save_uploaded_file(file)
-
 
         try:
             logger.info("Starting gene parsing...")
@@ -45,15 +60,15 @@ class FileProcessorService:
             # Parse genes y guarda en la base de datos simultáneamente
             async for genes_chunk in self.vcf_parser.parse_vcf(file_path):
                 total_genes += len(genes_chunk)
-                await self._process_chunk_parallel(genes_chunk, 0, 1, file_path)  # Procesar cada chunk inmediatamente
+                await self._process_chunk_parallel(genes_chunk)  # Procesar cada chunk inmediatamente
 
             logger.info(f"Total genes processed: {total_genes}")
 
             # Calculate processing time and speed
-            total_time = (datetime.now() - start_time).total_seconds()/60
+            total_time = (datetime.now() - start_time).total_seconds() / 60
 
             logger.info(f"Processing completed successfully in {total_time:.2f} min")
-            file_record = {"file_path": file_path, "total_genes": total_genes}  
+            file_record = {"file_path": file_path, "total_genes": total_genes}
             os.remove(file_path)  # Remover el archivo temporal
             return {"status": "success", "data": file_record}
 
@@ -61,15 +76,14 @@ class FileProcessorService:
             logger.error(f"Processing error: {str(e)}")
             return {"status": "error", "message": str(e)}
 
-    async def _process_chunk_parallel(self, chunk, chunk_index):
+    async def _process_chunk_parallel(self, chunk):
         """
         Process a single chunk of genes in parallel.
         
         :param chunk: Chunk of genes to process
-        :param chunk_index: Current chunk index
         """
         try:
             await self.genes_collection.insert_many([gene.model_dump() for gene in chunk], ordered=False)
         except Exception as e:
-            logger.error(f"Error inserting chunk {chunk_index + 1} into database: {str(e)}")
+            logger.error(f"Error inserting chunk into database: {str(e)}")
             raise
